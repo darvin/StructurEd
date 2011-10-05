e43f1da2f25af49f590894d56e3f90180175ecb7
Sergey Klimov <dcdarv@gmail.com>
2011-10-05 17:18:10 +0300

    xml format dummy

---
+++ src/unitreeserializer/xml_format.py
@@ -0,0 +1,18 @@
+from format import Format
+
+
+class XmlFormat(Format):
+    extensions = ["---"]
+    description = "Some Format"
+
+    @classmethod
+    def loads(cls, data_st):
+        raise NotImplementedError
+
+    @classmethod
+    def dumps(cls, data):
+        raise NotImplementedError
+
+    @classmethod
+    def is_available(cls):
+        return False
